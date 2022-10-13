14c3da208df86e3cdbc6787435347cd489afbf55
BEBECO <kbksky@gmail.com>
2022-10-13 21:55:24 +0900

    feat 082. Spring WebMVC 프레임워크 사용법

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardController.java
@@ -1,19 +1,23 @@
 package com.bitcamp.board.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import com.bitcamp.board.domain.AttachedFile;
 import com.bitcamp.board.domain.Board;
 import com.bitcamp.board.domain.Member;
@@ -24,39 +28,46 @@ import com.bitcamp.board.service.BoardService;
 @RequestMapping("/board/")
 public class BoardController {
 
+  ServletContext sc; // ServletContext객체는 생성자를 통해 주입 받아야! 파라미터로는 주입 놉
   BoardService boardService;
 
-  public BoardController(BoardService boardService) {
+  public BoardController(BoardService boardService, ServletContext sc) {
     this.boardService = boardService;
+    this.sc = sc;
+  }
+  // @InteranlResourceViewResolver 사용 전
+  //  @GetMapping("form")
+  //  public String form() throws Exception {
+  //    return "board/form";
+  //  }
+
+  // @InteranlResourceViewResolver 사용 전
+  @GetMapping("form")
+  public void form() throws Exception {
   }
 
-  @GetMapping("form")
-  public String form(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    return "/board/form.jsp";
-  }
 
   @PostMapping("add")
-  public String add(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  public String add(
+      Board  board,
+      @RequestParam("files") MultipartFile[] files,  
+      HttpSession session) throws Exception {
 
-    Board board = new Board();
-    board.setTitle(request.getParameter("title"));
-    board.setContent(request.getParameter("content"));
-    board.setAttachedFiles(saveAttachedFiles(request));
-    board.setWriter((Member) request.getSession().getAttribute("loginMember"));
+    board.setAttachedFiles(saveAttachedFiles(files));
+    board.setWriter((Member) session.getAttribute("loginMember"));
 
     // 서비스 객체에 업무를 맡긴다.
     boardService.add(board);
     return "redirect:list";
   }
 
-  private List<AttachedFile> saveAttachedFiles(HttpServletRequest request) 
+  private List<AttachedFile> saveAttachedFiles(Part[] files) 
       throws IOException, ServletException {
     List<AttachedFile> attachedFiles = new ArrayList<>();
-    String dirPath = request.getServletContext().getRealPath("/board/files");
-    Collection<Part> parts = request.getParts();
+    String dirPath = sc.getRealPath("/board/files");
 
-    for (Part part : parts) {
-      if (!part.getName().equals("files") || part.getSize() == 0) {
+    for (Part part : files) {
+      if (part.getSize() == 0) {
 
         continue;
       }
@@ -68,36 +79,51 @@ public class BoardController {
     return attachedFiles;
   }
 
+  private List<AttachedFile> saveAttachedFiles(MultipartFile[] files) 
+      throws IOException, ServletException {
+    List<AttachedFile> attachedFiles = new ArrayList<>();
+    String dirPath = sc.getRealPath("/board/files");
+
+    for (MultipartFile part : files) {
+      if (part.isEmpty()) {
+        continue;
+      }
+
+      String filename = UUID.randomUUID().toString();
+      part.transferTo(new File(dirPath + "/" + filename));
+      attachedFiles.add(new AttachedFile(filename));
+    }
+    return attachedFiles;
+  }
+
   @GetMapping("list")
-  public String list(HttpServletRequest req, HttpServletResponse resp)
-      throws Exception {
-    req.setAttribute("boards", boardService.list());
-    return "/board/list.jsp";
+  public void list(Model model) throws Exception {
+    model.addAttribute("boards", boardService.list());
+    //    mv.setViewName("board/list");
   }
 
   @GetMapping("detail")
-  public String detail(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int boardNo = Integer.parseInt(request.getParameter("no"));
-    Board board = boardService.get(boardNo);
+  public Map detail(int no) throws Exception {
+    Board board = boardService.get(no);
 
     if (board == null) {
       throw new Exception("해당 번호의 게시글이 없습니다!");
     }
-    request.setAttribute("board", board);
-    return "/board/detail.jsp";
+
+    Map map = new HashMap();
+    map.put("board", board);
+    return map;
   }
 
   @PostMapping("update")
-  public String update(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  public String update(
+      Board board, 
+      Part[] files, 
+      HttpSession session) throws Exception { 
 
-    Board board = new Board();
+    board.setAttachedFiles(saveAttachedFiles(files));
 
-    board.setNo(Integer.parseInt(request.getParameter("no")));
-    board.setTitle(request.getParameter("title"));
-    board.setContent(request.getParameter("content"));
-    board.setAttachedFiles(saveAttachedFiles(request));
-
-    checkOwner(board.getNo(), request.getSession());
+    checkOwner(board.getNo(), session);
 
     if (!boardService.update(board)) {
       throw new Exception("게시글을 변경할 수 없습니다!");
@@ -114,10 +140,11 @@ public class BoardController {
   }
 
   @GetMapping("delete")
-  public String delete(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int no = Integer.parseInt(request.getParameter("no"));
+  public String delete(
+      int no, 
+      HttpSession session) throws Exception {
 
-    checkOwner(no, request.getSession());
+    checkOwner(no, session);
 
     if (!boardService.delete(no)) {
       throw new Exception("게시글을 삭제할 수 없습니다.");
@@ -127,14 +154,15 @@ public class BoardController {
   }
 
   @GetMapping("fileDelete")
-  public String fileDelete(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    int no = Integer.parseInt(request.getParameter("no"));
+  public String fileDelete(
+      int no, 
+      HttpSession session) throws Exception {
 
     // 첨부파일 정보를 가져온다.
     AttachedFile attachedFile = boardService.getAttachedFile(no); 
 
     // 게시글의 작성자가 로그인 사용자인지 검사한다.
-    Member loginMember = (Member) request.getSession().getAttribute("loginMember");
+    Member loginMember = (Member) session.getAttribute("loginMember");
     Board board = boardService.get(attachedFile.getBoardNo()); 
 
     if (board.getWriter().getNo() != loginMember.getNo()) {
